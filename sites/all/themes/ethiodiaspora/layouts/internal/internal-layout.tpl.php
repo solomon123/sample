81bdc85d76a9b9ab03aae55a53b2f273ef74e0f3
solomon123 <solomonyif@gmail.com>
2017-03-10 14:34:22 -0500

    internal-layout.tpl update

--- sites/all/themes/ethiodiaspora/layouts/internal/internal-layout.tpl.php
+++ sites/all/themes/ethiodiaspora/layouts/internal/internal-layout.tpl.php
@@ -35,7 +35,7 @@
         
         <h1><?php //print $title; ?></h1>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
+      <?php //print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php //print render($tabs); ?>
       <?php print render($page['help']); ?>
